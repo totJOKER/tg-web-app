05b4c740e8095be4cf2962427dd09a7b1e37c36a
totJOKER <117646766+totJOKER@users.noreply.github.com>
2023-08-23 15:18:26 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/header/header';
 
 
 
 function App() {
-  const {onToggleButton, tg} = useTelegram();
-  useEffect(() => {
-    tg.ready();
-  }, [])
+    const {onToggleButton, tg} = useTelegram();
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
 
-  return (
-    <div className="App">
-      <button onClick={onToggleButton} >toggle</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <button onClick={onToggleButton} >toggle</button>
+        </div>
+    );
 }
 
 export default App;
